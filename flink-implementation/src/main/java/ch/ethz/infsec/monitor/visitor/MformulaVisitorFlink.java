3dee09990581d7c100e7a3827526d377cbd619ce
Emma Hedvig Pind Hansen <emmahedvig@hotmail.com>
2021-11-03 09:41:15 +0100

    partition stream

--- flink-implementation/src/main/java/ch/ethz/infsec/monitor/visitor/MformulaVisitorFlink.java
+++ flink-implementation/src/main/java/ch/ethz/infsec/monitor/visitor/MformulaVisitorFlink.java
@@ -4,6 +4,7 @@ import ch.ethz.infsec.monitor.Fact;
 import ch.ethz.infsec.policy.VariableID;
 import javassist.compiler.ast.Variable;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.Partitioner;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.datastream.*;
@@ -34,10 +35,10 @@ public class MformulaVisitorFlink implements MformulaVisitor<DataStream<Pipeline
         this.mainDataStream = mainDataStream;
     }
 
-    public DataStream<PipelineEvent> visit(MPred pred) {
-        OutputTag<Fact> factStream = this.hmap.get(pred.getPredName());
-        pred.setNumberProcessors(1);
-        return this.mainDataStream.getSideOutput(factStream).flatMap(pred).setParallelism(1);
+    public DataStream<PipelineEvent> visit(MPred f) {
+        OutputTag<Fact> factStream = this.hmap.get(f.getPredName());
+        f.setNumberProcessors(1);
+        return this.mainDataStream.getSideOutput(factStream).flatMap(f).setParallelism(1);
         // return this.mainDataStream.getSideOutput(factStream).flatMap(pred).setParallelism(Main.numberProcessors);
     }
 
@@ -50,132 +51,278 @@ public class MformulaVisitorFlink implements MformulaVisitor<DataStream<Pipeline
         // get common keys
         // List<VariableID> commonKeys = f.keys;
 
-        // get collisionless keys
-        scala.collection.mutable.HashMap<Object, Object> map = ColissionlessKeyGenerator
-                .getMapping(Main.numberProcessors);
-        Map<Object, Object> mapping = JavaConverters.mapAsJavaMap(map);
-        System.out.println(mapping);
-
         // flatmap to duplicate terminators and set key of events
         DataStream<PipelineEvent> input1duplicated = input1
-                .flatMap(new DuplicateTerminators(Main.numberProcessors, mapping))
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
                 .setParallelism(1);
-        // input1duplicated.writeAsText("/Users/emmahedvigpindhansen/Desktop/BA/my_project/flinkMonitor/res_and_input1duplicate", FileSystem.WriteMode.OVERWRITE);
         DataStream<PipelineEvent> input2duplicated = input2
-                .flatMap(new DuplicateTerminators(Main.numberProcessors, mapping))
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
                 .setParallelism(1);
-        // key stream
-        ConnectedStreams<PipelineEvent, PipelineEvent> connectedStreamsKeyed = input1
-                .keyBy(new KeySelector<PipelineEvent, Integer>() {
+        // partition data
+        ConnectedStreams<PipelineEvent, PipelineEvent> connectedStreams = input1.partitionCustom(new Partitioner<Integer>() {
+            @Override
+            public int partition(Integer key, int numPartitions) {
+                return key % numPartitions;
+            }}, new KeySelector<PipelineEvent, Integer>() {
                     @Override
                     public Integer getKey(PipelineEvent event) throws Exception {
                         return event.key;
                     }
                 })
                 .connect(
-                        input2.keyBy(new KeySelector<PipelineEvent, Integer>() {
-                            @Override
-                            public Integer getKey(PipelineEvent event) throws Exception {
-                                return event.key;
-                            }
+                        input2.partitionCustom(new Partitioner<Integer>() {
+                    @Override
+                    public int partition(Integer key, int numPartitions) {
+                        return key % numPartitions;
+                    }}, new KeySelector<PipelineEvent, Integer>() {
+                    @Override
+                    public Integer getKey(PipelineEvent event) throws Exception {
+                        return event.key;
+                    }
                 }));
-        // see result from keyed stream
-        KeyedStream<PipelineEvent, Integer> input1Keyed = input1duplicated
-                .keyBy(new KeySelector<PipelineEvent, Integer>() {
-                    @Override
-                    public Integer getKey(PipelineEvent event) throws Exception {
-                        return event.key;
-                    }
-                });
-        input1Keyed.writeAsText("/Users/emmahedvigpindhansen/Desktop/BA/my_project/flinkMonitor/res_and_1", FileSystem.WriteMode.OVERWRITE);
-        KeyedStream<PipelineEvent, Integer> input2Keyed = input2duplicated
-                .keyBy(new KeySelector<PipelineEvent, Integer>() {
-                    @Override
-                    public Integer getKey(PipelineEvent event) throws Exception {
-                        return event.key;
-                    }
-                });
-        input2Keyed.writeAsText("/Users/emmahedvigpindhansen/Desktop/BA/my_project/flinkMonitor/res_and_2", FileSystem.WriteMode.OVERWRITE);
-
-        // ConnectedStreams<PipelineEvent, PipelineEvent> connectedStreams = input1.connect(input2);
-        return connectedStreamsKeyed.flatMap(f).setParallelism(Main.numberProcessors);
+        return connectedStreams.flatMap(f).setParallelism(Main.numberProcessors);
     }
 
     public DataStream<PipelineEvent> visit(MExists f) {
         DataStream<PipelineEvent> input = f.subFormula.accept(this);
-        return input.flatMap(f).setParallelism(Main.numberProcessors);
+        f.setNumberProcessors(Main.numberProcessors);
+        // duplicate terminators
+        DataStream<PipelineEvent> inputduplicated = input
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        // partition data
+        DataStream<PipelineEvent> partitioned = inputduplicated.partitionCustom(new Partitioner<Integer>() {
+            @Override
+            public int partition(Integer key, int numPartitions) {
+                return key % numPartitions;
+            }
+        }, new KeySelector<PipelineEvent, Integer>() {
+            @Override
+            public Integer getKey(PipelineEvent event) throws Exception {
+                return event.key;
+            }
+        });
+        return partitioned.flatMap(f).setParallelism(Main.numberProcessors);
     }
 
     public DataStream<PipelineEvent> visit(MNext f) {
         DataStream<PipelineEvent> input = f.formula.accept(this);
-        return input.flatMap(f).setParallelism(Main.numberProcessors);
+        f.setNumberProcessors(Main.numberProcessors);
+        // duplicate terminators
+        DataStream<PipelineEvent> inputduplicated = input
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        // partition data
+        DataStream<PipelineEvent> partitioned = inputduplicated.partitionCustom(new Partitioner<Integer>() {
+            @Override
+            public int partition(Integer key, int numPartitions) {
+                return key % numPartitions;
+            }
+        }, new KeySelector<PipelineEvent, Integer>() {
+            @Override
+            public Integer getKey(PipelineEvent event) throws Exception {
+                return event.key;
+            }
+        });
+        return partitioned.flatMap(f).setParallelism(Main.numberProcessors);
     }
 
     public DataStream<PipelineEvent> visit(MOr f) {
         DataStream<PipelineEvent> input1 = f.op1.accept(this);
         DataStream<PipelineEvent> input2 = f.op2.accept(this);
-        ConnectedStreams<PipelineEvent, PipelineEvent> connectedStreams = input1.connect(input2);
+
+        f.setNumberProcessors(Main.numberProcessors);
+
+        // get common keys
+        // List<VariableID> commonKeys = f.keys;
+
+        // flatmap to duplicate terminators and set key of events
+        DataStream<PipelineEvent> input1duplicated = input1
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        DataStream<PipelineEvent> input2duplicated = input2
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        // partition data
+        ConnectedStreams<PipelineEvent, PipelineEvent> connectedStreams = input1.partitionCustom(new Partitioner<Integer>() {
+                    @Override
+                    public int partition(Integer key, int numPartitions) {
+                        return key % numPartitions;
+                    }}, new KeySelector<PipelineEvent, Integer>() {
+                    @Override
+                    public Integer getKey(PipelineEvent event) throws Exception {
+                        return event.key;
+                    }
+                })
+                .connect(
+                        input2.partitionCustom(new Partitioner<Integer>() {
+                            @Override
+                            public int partition(Integer key, int numPartitions) {
+                                return key % numPartitions;
+                            }}, new KeySelector<PipelineEvent, Integer>() {
+                            @Override
+                            public Integer getKey(PipelineEvent event) throws Exception {
+                                return event.key;
+                            }
+                        }));
         return connectedStreams.flatMap(f).setParallelism(Main.numberProcessors);
-        //flatMap here will be interpreted as a coflatmap because the argumetn it receives is an Or,
-        //which is a binary operator so it receives a coflatmap. This will apply flatMap1 or flatMap2 depending
-        //on the input stream
     }
 
     public DataStream<PipelineEvent> visit(MPrev f) {
         DataStream<PipelineEvent> input = f.formula.accept(this);
-        return input.flatMap(f).setParallelism(Main.numberProcessors);
+        f.setNumberProcessors(Main.numberProcessors);
+        // duplicate terminators
+        DataStream<PipelineEvent> inputduplicated = input
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        // partition data
+        DataStream<PipelineEvent> partitioned = inputduplicated.partitionCustom(new Partitioner<Integer>() {
+            @Override
+            public int partition(Integer key, int numPartitions) {
+                return key % numPartitions;
+            }
+        }, new KeySelector<PipelineEvent, Integer>() {
+            @Override
+            public Integer getKey(PipelineEvent event) throws Exception {
+                return event.key;
+            }
+        });
+        return partitioned.flatMap(f).setParallelism(Main.numberProcessors);
     }
 
     public DataStream<PipelineEvent> visit(MSince f) {
         DataStream<PipelineEvent> input1 = f.formula1.accept(this);
         DataStream<PipelineEvent> input2 = f.formula2.accept(this);
-        ConnectedStreams<PipelineEvent, PipelineEvent> connectedStreams = input1.connect(input2);
+
+        f.setNumberProcessors(Main.numberProcessors);
+
+        // get common keys
+        // List<VariableID> commonKeys = f.keys;
+
+        // flatmap to duplicate terminators and set key of events
+        DataStream<PipelineEvent> input1duplicated = input1
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        DataStream<PipelineEvent> input2duplicated = input2
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        // partition data
+        ConnectedStreams<PipelineEvent, PipelineEvent> connectedStreams = input1.partitionCustom(new Partitioner<Integer>() {
+                    @Override
+                    public int partition(Integer key, int numPartitions) {
+                        return key % numPartitions;
+                    }}, new KeySelector<PipelineEvent, Integer>() {
+                    @Override
+                    public Integer getKey(PipelineEvent event) throws Exception {
+                        return event.key;
+                    }
+                })
+                .connect(
+                        input2.partitionCustom(new Partitioner<Integer>() {
+                            @Override
+                            public int partition(Integer key, int numPartitions) {
+                                return key % numPartitions;
+                            }}, new KeySelector<PipelineEvent, Integer>() {
+                            @Override
+                            public Integer getKey(PipelineEvent event) throws Exception {
+                                return event.key;
+                            }
+                        }));
         return connectedStreams.flatMap(f).setParallelism(Main.numberProcessors);
     }
 
     public DataStream<PipelineEvent> visit(MUntil f) {
         DataStream<PipelineEvent> input1 = f.formula1.accept(this);
         DataStream<PipelineEvent> input2 = f.formula2.accept(this);
-        ConnectedStreams<PipelineEvent, PipelineEvent> connectedStreams = input1.connect(input2);
+
+        f.setNumberProcessors(Main.numberProcessors);
+
+        // get common keys
+        // List<VariableID> commonKeys = f.keys;
+
+        // flatmap to duplicate terminators and set key of events
+        DataStream<PipelineEvent> input1duplicated = input1
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        DataStream<PipelineEvent> input2duplicated = input2
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        // partition data
+        ConnectedStreams<PipelineEvent, PipelineEvent> connectedStreams = input1.partitionCustom(new Partitioner<Integer>() {
+                    @Override
+                    public int partition(Integer key, int numPartitions) {
+                        return key % numPartitions;
+                    }}, new KeySelector<PipelineEvent, Integer>() {
+                    @Override
+                    public Integer getKey(PipelineEvent event) throws Exception {
+                        return event.key;
+                    }
+                })
+                .connect(
+                        input2.partitionCustom(new Partitioner<Integer>() {
+                            @Override
+                            public int partition(Integer key, int numPartitions) {
+                                return key % numPartitions;
+                            }}, new KeySelector<PipelineEvent, Integer>() {
+                            @Override
+                            public Integer getKey(PipelineEvent event) throws Exception {
+                                return event.key;
+                            }
+                        }));
         return connectedStreams.flatMap(f).setParallelism(Main.numberProcessors);
     }
 
     @Override
     public DataStream<PipelineEvent> visit(MOnce f) {
         DataStream<PipelineEvent> input = f.formula.accept(this);
-
         f.setNumberProcessors(Main.numberProcessors);
-
-        // get collisionless keys
-        scala.collection.mutable.HashMap<Object, Object> map = ColissionlessKeyGenerator
-                .getMapping(Main.numberProcessors);
-        Map<Object, Object> mapping = JavaConverters.mapAsJavaMap(map);
-        System.out.println(mapping);
         // duplicate terminators
         DataStream<PipelineEvent> inputduplicated = input
-                .flatMap(new DuplicateTerminators(Main.numberProcessors, mapping))
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
                 .setParallelism(1);
-        // key stream
-        KeyedStream<PipelineEvent, Integer> inputKeyed = inputduplicated
-                .keyBy(new KeySelector<PipelineEvent, Integer>() {
-                    @Override
-                    public Integer getKey(PipelineEvent event) throws Exception {
-                        return event.key;
-                    }
-                });
-        inputKeyed.flatMap(f).setParallelism(Main.numberProcessors).writeAsText("/Users/emmahedvigpindhansen/Desktop/BA/my_project/flinkMonitor/res_once", FileSystem.WriteMode.OVERWRITE);
-        return inputKeyed.flatMap(f).setParallelism(Main.numberProcessors);
+        // partition data
+        DataStream<PipelineEvent> partitioned = inputduplicated.partitionCustom(new Partitioner<Integer>() {
+            @Override
+            public int partition(Integer key, int numPartitions) {
+                return key % numPartitions;
+            }
+        }, new KeySelector<PipelineEvent, Integer>() {
+            @Override
+            public Integer getKey(PipelineEvent event) throws Exception {
+                return event.key;
+            }
+        });
+        return partitioned.flatMap(f).setParallelism(Main.numberProcessors);
     }
 
     @Override
     public DataStream<PipelineEvent> visit(MEventually f) {
         DataStream<PipelineEvent> input = f.formula.accept(this);
-        return input.flatMap(f).setParallelism(Main.numberProcessors);
+        f.setNumberProcessors(Main.numberProcessors);
+        // duplicate terminators
+        DataStream<PipelineEvent> inputduplicated = input
+                .flatMap(new DuplicateTerminators(Main.numberProcessors))
+                .setParallelism(1);
+        // partition data
+        DataStream<PipelineEvent> partitioned = inputduplicated.partitionCustom(new Partitioner<Integer>() {
+            @Override
+            public int partition(Integer key, int numPartitions) {
+                return key % numPartitions;
+            }
+        }, new KeySelector<PipelineEvent, Integer>() {
+            @Override
+            public Integer getKey(PipelineEvent event) throws Exception {
+                return event.key;
+            }
+        });
+        return partitioned.flatMap(f).setParallelism(Main.numberProcessors);
     }
 
     public DataStream<PipelineEvent> visit(MRel f) {
         OutputTag<Fact> factStream = this.hmap.get("0Terminator");
-        return this.mainDataStream.getSideOutput(factStream).flatMap(f).setParallelism(Main.numberProcessors);
+        f.setNumberProcessors(1);
+        // return this.mainDataStream.getSideOutput(factStream).flatMap(f).setParallelism(Main.numberProcessors);
+        return this.mainDataStream.getSideOutput(factStream).flatMap(f).setParallelism(1);
     }
 
 }
--- flink-implementation/src/main/java/ch/ethz/infsec/util/DuplicateTerminators.java
+++ flink-implementation/src/main/java/ch/ethz/infsec/util/DuplicateTerminators.java
@@ -5,16 +5,15 @@ import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.util.Collector;
 import scala.collection.JavaConverters;
 
+import java.util.List;
 import java.util.Map;
 
 public class DuplicateTerminators implements FlatMapFunction<PipelineEvent, PipelineEvent> {
 
     int numberProcessors;
-    Map<Object, Object> mapping;
 
-    public DuplicateTerminators(int numberProcessors, Map<Object, Object> mapping) {
+    public DuplicateTerminators(int numberProcessors) {
         this.numberProcessors = numberProcessors;
-        this.mapping = mapping;
     }
 
     @Override
@@ -23,11 +22,11 @@ public class DuplicateTerminators implements FlatMapFunction<PipelineEvent, Pipe
         if (!event.isPresent()) {
             for (int i = 0; i < this.numberProcessors; i++) {
                 PipelineEvent temp = new PipelineEvent(event.getTimestamp(), event.getTimepoint(), true, event.get());
-                temp.key = (int) this.mapping.get(i);
+                temp.key = i;
                 out.collect(temp);
             }
         } else {
-            event.key = (int) this.mapping.get(0); // randomize this?? if so do from MformulaVisitorFlink
+            event.key = 0; // randomize this?? if so do from MformulaVisitorFlink
             out.collect(event);
         }
     }
